bbc1c81deaf05f705f5fd5fc739009c115bf4688
solene basaia <bsolene@hotmail.fr>
2018-05-03 23:26:58 +0200

    add test should return 2 when passed 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe "#fizzbuzz" do
     it "should return 1 when passed 1" do
         expect(fizzbuzz(1)).to eq(1)        
     end
+    it "should return 2 when passed 2" do
+        expect(fizzbuzz(2)).to eq(2) 
+    end
 end
