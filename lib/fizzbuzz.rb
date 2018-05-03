351f1f64bcee7cad98238b04fb5dae671281e3a6
solene basaia <bsolene@hotmail.fr>
2018-05-03 23:56:19 +0200

    pass the test should return FizzBuzz when passed 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number) 
-    if number % 5 == 0
+    if number % 5 == 0 && number % 3 == 0
+        "FizzBuzz"
+    elsif number % 5 == 0
         "Buzz"
     elsif number % 3 == 0
         "Fizz"
