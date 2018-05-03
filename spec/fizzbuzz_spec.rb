915318aec6e2c5fa43842b07b2d3cc88f81d3aee
solene basaia <bsolene@hotmail.fr>
2018-05-03 23:47:01 +0200

    add test should return Buzz when passed 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,13 +4,20 @@ describe "#fizzbuzz" do
     it "should return 1 when passed 1" do
         expect(fizzbuzz(1)).to eq(1)        
     end
+
     it "should return 2 when passed 2" do
         expect(fizzbuzz(2)).to eq(2) 
     end
+
     it "should return 'Fizz' when passed 3" do
         expect(fizzbuzz(3)).to eq("Fizz") 
     end
+
     it "should return 'Fizz' when passed 6" do
         expect(fizzbuzz(6)).to eq("Fizz") 
+    end
+
+    it "should return 'Buzz' when passed 5" do
+        expect(fizzbuzz(5)).to eq("Buzz") 
     end
 end
