c377bd7106458a8d79475e5ba4e6fd3c82355a02
solene basaia <bsolene@hotmail.fr>
2018-05-04 00:07:45 +0200

    refactoring the method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,23 @@
 def fizzbuzz(number) 
-    if number % 5 == 0 && number % 3 == 0
+    if is_divisible_by_3_and_5(number)
         "FizzBuzz"
-    elsif number % 5 == 0
+    elsif is_divisible_by_5?(number)
         "Buzz"
-    elsif number % 3 == 0
+    elsif is_divisible_by_3?(number)
         "Fizz"
     else
         number
     end
+end
+
+def is_divisible_by_3?(number)
+    number % 3 == 0
+end
+
+def is_divisible_by_5?(number)
+    number % 5 == 0
+end
+
+def is_divisible_by_3_and_5(number)
+    is_divisible_by_5?(number) && is_divisible_by_3?(number)
 end
